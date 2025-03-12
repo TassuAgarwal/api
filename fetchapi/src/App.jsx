8adc49549cb9da991da04d681051205399b12998
Tassu <tassuagarwal003@gmail.com>
2025-03-12 10:33:51 +0530

    New update

--- fetchapi/src/App.jsx
+++ fetchapi/src/App.jsx
@@ -15,15 +15,15 @@ function App() {
     <>
       {data ? data.map((e) =>
         <Productlist
-        
+          id={e.id}
           title={e.title}
           image={e.image}
           desc={e.description}
           category={e.category}
           price={e.price}
           rating={{
-            rate: e.rate,
-            count: e.count
+            rate: e.rating?.rate,
+            count: e.rating?.count
           }}
 
         />) : <p> No data</p>}
--- fetchapi/src/components/Productlist.jsx
+++ fetchapi/src/components/Productlist.jsx
@@ -4,29 +4,33 @@ const Productlist = (props) => {
   return (
 
     <>
-    {/* <div className="row"> */}
+      {/* <div className="row"> */}
       <div className="col">
-      <div className="product-card">
-        <div className="badge">Hot</div>
-        <div className="product-tumb">
-          <img src={props.image} alt="producr" />
-        </div>
-        <div className="product-details">
-          <span className="product-catagory">{props.category}</span>
-          <h4><a href="">{props.title}</a></h4>
-          <p>{props.desc}</p>
-          <div className="product-bottom-details">
-            <div className="product-price">${props.price}</div>
-            <div className="product-links">
-              <a href=""><i className="fa fa-heart"></i></a>
-              <a href=""><i className="fa fa-shopping-cart"></i></a>
+        <div className="product-card">
+          <div className="badge">{props.id}</div>
+          <div className="product-tumb">
+            <img src={props.image} alt="producr" />
+          </div>
+          <div className="product-details">
+            <span className="product-catagory">{props.category}</span>
+            <h4><a href="">{props.title}</a></h4>
+            <p>{props.desc}</p>
+            <div className="product-bottom-details">
+              <div className="product-price">${props.price}</div>
+              <div className="product-links">
+                <div className="product-rating">
+                  <span>{props.rating?.rate}/5</span><br />
+                  <span>{props.rating?.count}</span>
+                </div>
+                <a href=""></a>
+                <a href=""></a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      </div>
-    {/* </div> */}
-      
+      {/* </div> */}
+
     </>
   )
 };
